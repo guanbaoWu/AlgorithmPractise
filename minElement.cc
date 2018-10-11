c2aebefaa25aed4ab9c309cf07ec3008448a2f04
wuguanbao <wuguanbaolj@126.com>
2018-10-11 23:39:20 +0800

    一天一算法之旋转数组的最小数字
    1、解决编译失败的问题
    2、解决边界问题导致算法死循环的问题

--- minElement.cc
+++ minElement.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -15,9 +16,10 @@ public:
 	{
 		int start = 0, end = length-1, min = 0;
 		
-		if (NULL == arry || 0 <= length)
+		if (NULL == arry || 0 >= length)
 		{
-			throw new exception("invalid args");
+			logic_error ex("invalid args");
+			throw new exception(ex);
 		}
 		
 		/**
@@ -34,7 +36,7 @@ public:
 			}
 			
 			
-			min = (end + start) >> 2;
+			min = (end + start) >> 1;
 			
 			/**
 			* 三个元素相同,此时无法确认中间元素归属于那个递增子数组,只能遍历查找
@@ -53,7 +55,7 @@ public:
 			/**
 			* 中间元素还在第二个递增子数组中,移动end
 			**/
-			else if(arry[min] >= arry[end])
+			else if(arry[min] <= arry[end])
 			{
 				end = min;
 			}
@@ -68,7 +70,8 @@ public:
 		
 		if (NULL == arry)
 		{
-			throw new exception("invalid args");
+			logic_error ex("invalid args");
+			throw new exception(ex);
 		}
 		
 		
@@ -85,3 +88,18 @@ public:
 
 	}
 };
+
+int main()
+{
+	int arry1[] = {9,23,26,2,3,7};
+	int arry2[] = {1,3,5,7,9,10};
+	int arry3[] = {1,0,1,1,1,1};
+	
+	Solution s;
+	
+	cout << "min element:"<< s.MinElement(arry1, sizeof(arry1)/sizeof(arry1[0])) << endl;
+	cout << "min element:"<< s.MinElement(arry2, sizeof(arry2)/sizeof(arry2[0])) << endl;
+	cout << "min element:"<< s.MinElement(arry3, sizeof(arry3)/sizeof(arry3[0])) << endl;
+
+	return 0;
+}
