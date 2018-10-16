716ae764ada8fe8f1a52723a08fdb3ab298ee9fc
wuguanbao <wuguanbaolj@126.com>
2018-10-16 22:21:49 +0800

    一天一算法之机器人移动路径
    1、编译通过，简单测试例子验证

--- movingCount.cc
+++ movingCount.cc
@@ -15,9 +15,9 @@ public:
 	* @ matrix：路径矩阵
 	* @ sum：要求坐标之和
 	**/
-	int movingCount(int matrix[][], int row,int col, int sum)
+	int movingCount(int row,int col, int sum)
 	{
-		if (NULL == matrix || row <= || col <= 0)
+		if (row <= 0 || col <= 0)
 		{
 			return 0;
 		}
@@ -25,7 +25,7 @@ public:
 		/**
 		* 创建一个用于标志走过路径的二维数组
 		**/
-		bool visited = new bool[row][col]
+		bool *visited = new bool[row*col];
 		for (int i = 0; i < row*col; ++i)
 		{
 			visited[i] = false;
@@ -34,7 +34,7 @@ public:
 		/**
 		* 从坐标(0,0)开始搜索
 		**/
-		int count = Search(matrix, row, col, 0, 0, sum, visited);
+		int count = Search(row, col, 0, 0, sum, visited);
 		
 		delete [] visited;
 		
@@ -42,9 +42,9 @@ public:
 
 	}
 	
-	int Search(int matrix[][], int rows, int cols, int rowIndex, int colIndex, int sum, bool visited[][])
+	int Search(int rows, int cols, int rowIndex, int colIndex, int sum, bool *visited)
 	{
-		if (NULL == matrix || row <= || col <= 0)
+		if (rows <= 0 || cols <= 0 || NULL == visited)
 		{
 			return 0;
 		}
@@ -58,14 +58,14 @@ public:
 		* 未访问过,且满足条件,移动
 		**/
 		int count = 0;
-		if (!visited[rowIndex][colIndex] && IsRight(matrix, rows, cols, rowIndex, colIndex, sum))
+		if (IsRight(rows, cols, rowIndex, colIndex, sum) && !visited[rowIndex*cols+colIndex])
 		{
-			visited[rowIndex][colIndex] = true;
+			visited[rowIndex*cols+colIndex] = true;
 			
 			count += 1;
-			for (int i = 0; i < 4, ++i)
+			for (int i = 0; i < 4; ++i)
 			{
-				count += Search(matrix, rows, cols, 
+				count += Search(rows, cols, 
 							rowIndex + direct[i][0], colIndex + direct[i][1], sum, visited);
 			}
 		}
@@ -73,11 +73,11 @@ public:
 		return count;
 	}
 
-	bool IsRight(int matrix[][], int rows, int cols, int rowIndex, int colIndex, int sum)
+	bool IsRight(int rows, int cols, int rowIndex, int colIndex, int sum)
 	{
 		
 		if (rowIndex >= 0 && rowIndex < rows && colIndex >= 0 && colIndex < cols
-			&& getSum(rowIndex) + getSum(colIndex) <= colIndex)
+			&& getSum(rowIndex) + getSum(colIndex) <= sum)
 		{
 			return true;
 		}
@@ -91,9 +91,25 @@ public:
 		while(number > 0)
 		{
 			sum += number%10;
-			number /= number;
+			number /= 10;
 		}
 		
 		return sum;
 	}
 };
+
+int main()
+{
+	int matrix[][4] = {{0,1,2,3},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+	int k = 0;
+	
+	Solution s;
+	
+	cout << "input number:" << endl;
+	cin >> k;
+	
+	cout << "k=" << " " << k << " " << "path count:" << " " << s.movingCount(4,4,k);
+	
+	return 0;
+	
+}
