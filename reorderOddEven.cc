59007b79d7ba98213b0529bae789694400627c77
wuguanbao <wuguanbaolj@126.com>
2018-11-06 22:36:54 +0800

    一天一算法之奇数在偶数前
    1、实现整形数组中，奇数在偶数的算法，算法复杂度为O(n)
    2、编译测试通过

--- reorderOddEven.cc
+++ reorderOddEven.cc
@@ -22,8 +22,9 @@ public:
         * O(n^2)的解法,遍历数组,偶数元素取出,移动后面的数字,将该元素放置在空位
         **/
         int even = 0;
-        int i = 0;
-        int size = length;
+        unsigned int i = 0;
+		unsigned int j = 0;
+        unsigned int size = length;
         while(size > 0)
         {
             --size;
@@ -35,7 +36,7 @@ public:
             {
                 even = pArry[i];
                 
-                for(int j = i; j < length - 1; ++j)
+                for(j = i; j < length - 1; ++j)
                 {
                     pArry[j] = pArry[j+1]; 
                 }
@@ -49,19 +50,65 @@ public:
         }
         
     }
+    
+    /**
+    * O(n)的解法
+    **/
+    bool bIsEven(int num)
+    {
+        if (num & 1)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    void ReOrderOddEven(int *pArry, unsigned int length, bool boptimal)
+    {
+        if (NULL == pArry)
+        {
+            return;
+        }
+        
+        /**
+        * O(n)的解法,增加两个指针,一个指向头,一个指向尾巴,当不满足要求时,既进行交换
+        **/
+        int *start = &pArry[0];
+        int *end = &pArry[length - 1];
+        
+        while(start < end)
+        {
+            while(end > start && !bIsEven(*end)) --end;
+            while(start < end && bIsEven(*start)) ++start;
+            
+            if (start < end)
+            {
+                int temp = *start;
+                *start = *end;
+                *end = temp;
+            }
+        }
+    }
 };
 
 int main()
 {
     int a[7] = {1,3,2,5,8,11,12};
+    int b[7] = {1,32,11,24,8,11,12};
     
     Solution s;
     
     s.ReOrderOddEven(a, sizeof(a)/sizeof(a[0]));
+    s.ReOrderOddEven(b, sizeof(b)/sizeof(b[0]), true);
     
-    for(int i = 0; i < sizeof(a)/sizeof(a[0]); ++i)
+    for(unsigned int i = 0; i < sizeof(a)/sizeof(a[0]); ++i)
     {
         printf("[%d]:%d\n", i, a[i]);
+    }
+    
+    for(unsigned int i = 0; i < sizeof(b)/sizeof(b[0]); ++i)
+    {
+        printf("[%d]:%d\n", i, b[i]);
     }
     
     return 0;
